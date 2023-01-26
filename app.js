7748e5df639c0a76bb9dcf4953caa7f3bbe9f20b
amdadul-haque <amdadul1807107@gmail.com>
2023-01-26 22:03:13 +0600

    design done, fully responsive

---
+++ app.js
@@ -0,0 +1,11 @@
+const hamberberMenu = document.getElementById("hamberger");
+const nav = document.getElementById("nav");
+const crosBtn = document.getElementById("crossBtn");
+
+hamberberMenu.addEventListener('click', ()=>{
+  nav.classList.toggle('hideNav')
+})
+crosBtn.addEventListener('click', ()=>{
+  nav.classList.add('hideNav')
+})
+
