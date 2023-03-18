1d62c9b77f0e31a40048ed34da19bd7c1a4b5f1c
amdadul-haque <amdadul1807107@gmail.com>
2023-03-19 00:42:21 +0600

    down page partialy done

--- team.js
+++ team.js
@@ -1,7 +1,7 @@
 const teamContainer = document.getElementById('team-container')
 
 const teamMembers = [
-  { imageSrc: "img/team/fraser.png", name: "Fraser Smith", designation: "Founder & CTO", twitter: "https://twitter.com/frankiebarns", linkedin: "https://www.linkedin.com/in/frasersmith/", details: "An accomplished Cloud Systems Engineer and Consultant, Fraser comes with more than 10 years of tenure in IT, with focused experience in Sales, Management and Engineering. Most recently he has worked in application development for foreign exchanges and leading Australian banks and has been an advocate of cryptocurrencies since 2017." },
+  { imageSrc: "img/team/fraser.png", name: "Fraser Smith", designation: "Founder & CTO", twitter: "https://twitter.com/frankiebarns", linkedin: "https://www.linkedin.com/in/frasersmith/", github: "https://github.com", details: "An accomplished Cloud Systems Engineer and Consultant, Fraser comes with more than 10 years of tenure in IT, with focused experience in Sales, Management and Engineering. Most recently he has worked in application development for foreign exchanges and leading Australian banks and has been an advocate of cryptocurrencies since 2017." },
   { imageSrc: "img/team/victor.jpg", name: "Victor Weibe", designation: "Blockchain Advisor", twitter: "https://github.com/onigiri-x/", linkedin: "https://www.linkedin.com/in/victorwiebe1/", details: "Victor joins us with more than 10 years of experience in software development, having extensive involvement building web3 products on Solidity and Ethereum networks since 2017; across a range of products including cryptocurrency exchanges, DeFi projects currently listed on Binance and Coinbase, and several NFT projects." },
   { imageSrc: "img/team/xavier.jpg", name: "Xavier O'Farrel", designation: "Data Engineer", twitter: "", linkedin: "https://www.linkedin.com/in/xavier-o-farrell-a8915016b/", details: "Xavier is an extremely gifted programmer, having worked closely in quantitative analysis and data modelling, he brings to the team a solid foundation of experience in Python and R." },
   { imageSrc: "img/team/julian.jpg", name: "Julian Loth ", designation: "Social Manager", twitter: "", linkedin: "https://www.linkedin.com/in/julian-loth-0b12b31a8/", details: "Bio" },
@@ -60,12 +60,23 @@ teamMembers.forEach(member => {
         <p>${member.details}</p>
       </div>
       <div class="popup-icons">
+      ${member.twitter ? `
         <div>
           <a href=${member.twitter} target="_blank"><i class="fab fa-twitter"></i></a>
         </div>
+        ` : ''}
+        
+        ${member.linkedin ? `
         <div>
           <a href=${member.linkedin} target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
         </div>
+        ` : ''}
+        ${member.github ? `
+        <div>
+          <a href=${member.github} target="_blank"><i class="fa-brands fa-github"></i></a>
+        </div>
+        ` : ''}
+        
       </div>
     </div>`
 
