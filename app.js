a3857ec2ad99dc42105094227016fa36d635a94e
Fraser <fraser.carter@gmail.com>
2023-04-03 19:00:50 -0500

    fixed menu

--- app.js
+++ app.js
@@ -6,19 +6,22 @@ const currentPos = 00;
 let pos = currentPos;
 var id = null
 
-const fullHeight = 300;
+const fullHeight = 320;
 const speed = 4;
 
 fetch('./header.html')
   .then(response => response.text())
   .then(data => {
     headerDiv.innerHTML = data;
+    const main = document.getElementById("main")
     const menuBtn = document.getElementById("menuBtn")
     const menuContentOuter = document.getElementById("menuContent-outer")
     const menuOuter = document.getElementById("menu-outer")
     const menuLinks = document.querySelectorAll('.menu-links');
 
     menuBtn.addEventListener('click', () => {
+      menuBtn.innerHTML = "Menu";
+      menuBtn.classList.remove("close-menu-btn");
       if (menuHeight) {
         menuHeight = false
         setTimeout(() => {
@@ -38,6 +41,8 @@ fetch('./header.html')
 
       }
       else {
+        menuBtn.innerHTML = "X";
+        menuBtn.classList.add("close-menu-btn");
         menuOuter.style.opacity = 0;
         menuOuter.style.display = 'block'
         menuHeight = true
@@ -52,10 +57,31 @@ fetch('./header.html')
           }
         }, 10)
       }
+      console.log(menuHeight)
       console.log(pos)
 
     })
-
+    menuOuter.addEventListener('click',()=>{
+      menuBtn.innerHTML = "Menu";
+      menuBtn.classList.remove("close-menu-btn");
+      menuHeight = false
+        setTimeout(() => {
+          menuOuter.style.opacity = 1;
+          menuOuter.style.display = 'none'
+        }, 1000)
+        clearInterval(id);
+        id = setInterval(() => {
+          if (pos <= currentPos) {
+            clearInterval(id);
+          } else {
+            pos -= speed;
+            menuContentOuter.style.height = `${pos}px`
+            menuOuter.style.opacity = parseFloat(menuOuter.style.opacity) - 0.1;
+          }
+        }, 10)
+      console.log(menuHeight)
+      console.log(pos)
+    })
     menuLinks.forEach((menuLink) => {
       menuLink.addEventListener('click', () => {
         menuOuter.classList.toggle('menu-outer-blur');
@@ -65,7 +91,7 @@ fetch('./header.html')
       })
     })
   });
-
+  
 fetch('./footer.html')
   .then(response => response.text())
   .then(data => {
